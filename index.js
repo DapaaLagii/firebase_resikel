b639be8a6141cfea352e429a9cea454db08b18c0
DapaaLagii <nandriadaffa02@gmail.com>
2024-12-17 13:12:10 +0700

    Add resikel_servicekey.json to .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-// Inisialisasi Firebase dengan Service Account Key
 initializeApp({
-  credential: cert('/resikel_servicekey.json'), // Path ke file service account
+  credential: cert('/resikel_servicekey.json'),
 });
 
 const db = getFirestore();
